9e9b8a87168b9196243f1cdf7dea95b6b981f1c5
Gaabriel Ponomarjov <ponomarjov.gaabriel@gmail.com>
2025-04-21 14:23:35 +0300

    SPB init

---
+++ src/test/java/com/example/dbarticle/DbArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
